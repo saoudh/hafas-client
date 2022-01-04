51f2b39da5b908f81984c7f9fd9b6bd44c609cea
Hussam Saoud <hs2012@outlook.com>
2022-01-04 12:32:25 +0100

    rmv now parses ticket price

--- p/rmv/index.js
+++ p/rmv/index.js
@@ -2,6 +2,13 @@
 
 const baseProfile = require('./base.json')
 const products = require('./products')
+const _parseJourney = require('../../parse/journey')
+const { parseHook } = require('../../lib/profile-hooks')
+
+const parseJourneyWithTickets = ({ parsed }, j) => {
+	const parsedCpy = { ...parsed, price: j.trfRes?.totalPrice?.amount };
+	return parsedCpy;
+}
 
 const hvvProfile = {
 	...baseProfile,
@@ -9,6 +16,7 @@ const hvvProfile = {
 	timezone: 'Europe/Berlin',
 
 	products,
+	parseJourney: parseHook(_parseJourney, parseJourneyWithTickets),
 
 	departuresGetPasslist: false,
 	departuresStbFltrEquiv: false,
